f6f683f5906cfb60ef7bace59fe9e56f9457a272
roncode <ron_benporat@yahoo.com>
2018-02-23 00:51:23 +0000

    validation for caption and add time stamp to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true #each post must have an image
   validates :user_id, presence: true #each post must have an associated user
+  validates :content, presence: true, length: { minimum: 4, maximum: 300 }
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
